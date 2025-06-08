cd115cd6c7beb99fbd213d2b6ae5273c77910f8e
Samuel Orellana Mateo <samuel.orellanamateo@duke.edu>
2025-06-08 16:41:15 +0200

    Select identifiers and customizable chart - v0.2.0

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900">
       <header className="fixed top-0 left-0 right-0 bg-white shadow z-10">
         <div className="max-w-screen-2xl mx-auto px-4 md:px-6 py-4">
-          <h1 className="text-2xl font-bold tracking-tight">Sickle Cell - Dashboard 0.1.0</h1>
+          <h1 className="text-2xl font-bold tracking-tight">Sickle Cell - Dashboard 0.2.0</h1>
         </div>
       </header>
       <main className="flex-1 pt-20 px-4 md:px-6 pb-10 w-full max-w-screen-2xl mx-auto overflow-y-auto">
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['modest-exactly-asp.ngrok-free.app', 'localhost', '127.0.0.1', '0.0.0.0'],
+  },
   plugins: [react(), tailwindcss()],
 })
