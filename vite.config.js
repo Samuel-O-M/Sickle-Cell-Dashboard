ca8efad3e67b8696330321c825b6e61ffcee4d0c
Samuel Orellana Mateo <samuel.orellanamateo@duke.edu>
2026-02-18 10:48:25 -0500

    package.json and vite.config updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Sickle-Cell-Dashboard/",
   server: {
     allowedHosts: ['modest-exactly-asp.ngrok-free.app', 'localhost', '127.0.0.1', '0.0.0.0'],
   },
